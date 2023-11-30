dcc1caa8d629bb91568519edf1ccdea004053082
NecroBread <mariovyordanov@gmail.com>
2023-11-30 17:27:51 +0200

    minor changes

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,7 +6,7 @@ export default function Profile() {
   const session = useSession();
 
   if (session.data?.user) {
-    return <div>From client: user is : {JSON.stringify(session)}</div>;
+    return <div>From client: user is: {JSON.stringify(session)}</div>;
   }
 
   return <div>Nope</div>;
